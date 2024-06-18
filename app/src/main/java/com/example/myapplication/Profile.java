f3554b71c3f50e0eb3c08f4e9207849aee16e652
kitare17 <hoctapak@gmail.com>
2024-06-18 18:05:18 +0700

    fix update

--- app/src/main/java/com/example/myapplication/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -3,14 +3,18 @@ package com.example.myapplication;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.database.UserInfoRepository;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class Profile extends Fragment {
@@ -21,6 +25,13 @@ public class Profile extends Fragment {
 
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -28,6 +39,9 @@ public class Profile extends Fragment {
         fullnameEditText = (EditText) view.findViewById(R.id.fullnameEdit);
         SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         String googleId = sharedPreferences.getString("googleId", "googleId");
+
+
+
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +52,12 @@ public class Profile extends Fragment {
                 userInfoRepository.updateProfile(googleId, fullname);
                 sharedPreferences.edit().putString("fullname", fullname).apply();
 
+                NavigationView navigationView = (NavigationView) ((MainActivity)getContext()).findViewById(R.id.nav_view);
+                View headerView = navigationView.getHeaderView(0); // 0-indexed
+
+// Find the TextView inside the header layout
+                TextView headerTextView = headerView.findViewById(R.id.fullnameSlideBar);
+                headerTextView.setText(fullname);
 
             }
         });
